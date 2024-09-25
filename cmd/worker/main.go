4dc75b2ddab3be3f92142bf9638ed036aa1cd526
dgroux <dg.roux@gmail.com>
2024-09-25 21:26:18 +0200

    Update main.go

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -1,13 +1,5 @@
 package main
 
-import (
-	"github.com/feight/newsteam-sdk"
-	"github.com/feight/newsteam-sdk/importers/cosmos"
-)
-
 func main() {
-	newsteam.InitializeFeeds([]newsteam.Feed{
-		&cosmos.Importer{Feed: "bl", Host: "https://businesslive.co.za/apiv1", AccessToken: "348c758db9458109244ddbefe4549bde73133324"},
-		&cosmos.Importer{Feed: "bd", Host: "https://businesslive.co.za/apiv1", AccessToken: "348c758db9458109244ddbefe4549bde73133324"},
-	})
+
 }
